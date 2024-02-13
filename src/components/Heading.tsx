51830a1b229d978b7e5e0e09ee4883d8d2c1ac2a
gumokwe022 <124253391+Ms-P-Hub@users.noreply.github.com>
2024-02-13 23:23:46 +0200

    GRD-004 - Removed w-screen

--- src/components/Heading.tsx
+++ src/components/Heading.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Heading: React.FC = () => {
   return (
-    <header className="p-20 flex items-center justify-center w-screen w-full">
+  <header className="p-20 flex items-center justify-center w-full">
       <h1 className="text-9xl font-bold justify text-white">
         <span className="text-purple">G</span>ne
         <span className="text-green">R</span>d Vault
