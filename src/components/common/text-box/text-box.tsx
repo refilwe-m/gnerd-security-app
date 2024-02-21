8dfae3618b807d0ca2bc13d5ed2bf7f70f797da2
refilwe-m <refilwe.dev@gmail.com>
2024-02-21 17:06:55 +0200

    GRD-017 - Clipboard action

--- src/components/common/text-box/text-box.tsx
+++ src/components/common/text-box/text-box.tsx
@@ -1,15 +1,34 @@
-import { FC } from "react";
+import { FC, useState } from "react";
+import { ImCopy } from "react-icons/im";
+import { Button } from "..";
 
 type TextBoxProps = {
   text: string | number;
   label: string;
 };
 
-export const TextBox: FC<TextBoxProps> = ({ text, label }) => (
-  <section className="flex items-center gap-4 w-full">
-    <label className="text-xs w-2/5 font-bold">{label}</label>
-    <p className="border w-full text-xs rounded-lg py-2 px-3 ring-2 ring-primary outline-none">
-      {text}
-    </p>
-  </section>
-);
+export const TextBox: FC<TextBoxProps> = ({ text, label }) => {
+  const [copied, setCopied] = useState(false);
+  const copyToClipboard = () => {
+    setCopied(true);
+    navigator.clipboard.writeText(text.toString());
+  };
+  return (
+    <section className="flex items-center gap-4 w-full">
+      <label className="text-xs w-2/5 font-bold">{label}</label>
+      <section className="border w-full flex justify-between items-center text-xs rounded-lg py-2 px-3 ring-2 ring-primary outline-none">
+        <p>{text}</p>
+        <section>
+          <Button onClick={copyToClipboard} variant="text">
+            <ImCopy
+              onClick={copyToClipboard}
+              className={`hover:text-primary hover:text-base ${
+                copied ? "text-green" : "text-black"
+              }`}
+            />
+          </Button>
+        </section>
+      </section>
+    </section>
+  );
+};
